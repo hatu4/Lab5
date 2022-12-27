4a872edef5f8e5a2f57d5dfc1ac54506f0d2996c
Komp <Komp@DESKTOP-EREETD8>
2022-12-27 16:54:23 +0300

    Добавьте файлы проекта.

---
+++ FiguresLibrary/Triangle.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace FiguresLibrary
+{
+    public class Triangle : Polygon
+    {
+        public Triangle(int id, Point[] points, string u_name=null) : base(id, points)
+        {
+            unique_name = u_name;
+            name = "Треугольник";
+        }
+    }
+}
---
+++ FiguresLibrary/Triangle_points.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+
+namespace FiguresLibrary
+{
+    public class Triangle_points
+    {
+        public string name;
+        public Point[] points = new Point[3];
+        public Triangle_points(string name, int x1, int x2, int x3, int y1, int y2, int y3)
+        {
+            this.name = name;
+            this.points[0].X = x1;
+            this.points[0].Y = y1;
+            this.points[1].X = x2;
+            this.points[1].Y = y2;
+            this.points[2].X = x3;
+            this.points[2].Y = y3;
+        }
+        public void ResetPoints(int x1, int x2, int x3, int y1, int y2, int y3)
+        {
+            this.points[0].X = x1;
+            this.points[0].Y = y1;
+            this.points[1].X = x2;
+            this.points[1].Y = y2;
+            this.points[2].X = x3;
+            this.points[2].Y = y3;
+        }
+    }
+}
